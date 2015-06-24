39d593d300cfa373d8d10c08c706b6600e2b26ef
andrea-manzi <andrea.manzi@gmail.com>
2015-06-24 10:17:40 +0200

    added all:datamanagement to Public role

--- code/manifests/config.pp
+++ code/manifests/config.pp
@@ -64,7 +64,7 @@ class fts::config (
   fts3config{'/Infosys':             value => $bdii_infosys}
   fts3config{'/Alias':               value => $host_alias}
   fts3config{'/MonitoringMessaging': value => 'true'}
-  fts3config{'roles/Public':         value => 'vo:transfer;all:config'}
+  fts3config{'roles/Public':         value => 'vo:transfer;all:config;all:datamanagement'}
   fts3config{'roles/production':     value => 'all:config'}
   fts3config{'roles/lcgadmin':       value => 'all:config;vo:transfer'}
 
