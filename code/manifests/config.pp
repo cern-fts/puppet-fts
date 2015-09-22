2bcbe127d4b39897a6307226ac3c4ced06a81a0b
Steve Traylen <steve.traylen@cern.ch>
2015-09-22 11:28:18 +0200

    commit

--- code/manifests/config.pp
+++ code/manifests/config.pp
@@ -127,7 +127,7 @@ class fts::config (
     hour    => fqdn_rand(24),
     minute  => fqdn_rand(60),
     user    => root,
-    command => '/usr/sbin/tmpwatch -mc 10d /var/log/fts3/[0-9]*'
+    command => '/usr/sbin/tmpwatch -mc 3d /var/log/fts3/[0-9]*'
   }
 
 }
